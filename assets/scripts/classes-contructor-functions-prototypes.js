e4cbd1a5d5056630b0cef230279f82f00989a5b7
Shahrukh <shahrukh1494@gmail.com>
2023-09-20 13:33:14 +0530

    methods in classes and in constructors

--- assets/scripts/classes-contructor-functions-prototypes.js
+++ assets/scripts/classes-contructor-functions-prototypes.js
@@ -1,37 +1,46 @@
-// class AgedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
 
-// class Person extends AgedPerson {
-//   name = 'Max';
+class Person {
+  name = "Max";
 
-//   constructor() {
-//     super();
-//     this.age = 30;
-//   }
+  constructor() {
+    // super();
+    this.age = 30;
+    // this.greet = function() { ... }
+  }
 
-//   greet() {
-//     console.log(
-//       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-//     );
-//   }
-// }
+  // greet = () => {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // };
 
-function Person() {
-  this.age = 30;
-  this.name = "Max";
-  this.greet = function () {
+  greet() {
     console.log(
       "Hi, I am " + this.name + " and I am " + this.age + " years old."
     );
-  };
+  }
 }
 
-Person.describe = function () {
-  console.log("Creating persons...");
-};
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Max';
+//   // this.greet = function() { ... };
+// }
+
+// Person.prototype.greet = function() {
+//   console.log(
+//     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//   );
+// };
+
+// Person.describe = function() {
+//   console.log('Creating persons...');
+// }
 
 // Person.prototype = {
 //   printAge() {
@@ -39,16 +48,24 @@ Person.describe = function () {
 //   }
 // };
 
-Person.prototype.printAge = function () {
-  console.log(this.age);
-};
+// Person.prototype.printAge = function() {
+//   console.log(this.age);
+// };
 
-console.dir(Person);
+// console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.length);
+// console.log(p.toString());
+// const p2 = new p.__proto__.constructor();
+// console.dir(Object.prototype.__proto__);
 
 const p = new Person();
+const p2 = new Person();
 p.greet();
-p.printAge();
-console.log(p.length);
-console.log(p.toString());
-const p2 = new p.__proto__.constructor();
-console.dir(Object.prototype.__proto__);
+console.log(p);
+
+const button = document.getElementById("btn");
+button.addEventListener("click", p.greet.bind(p));
