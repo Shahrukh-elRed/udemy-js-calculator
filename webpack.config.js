e48dd3760210c03d68e72213c89dcdeab0625f1a
Shahrukh <shahrukh1494@gmail.com>
2023-09-22 19:25:29 +0530

    completed browser support, polyfills and traspiling code

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,18 @@ module.exports = {
   //   contentBase: "./",
   // },
   // plugins: [new CleanPlugin.CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
 };
